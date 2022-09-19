1f4c4a27c0fd493998b576ed33304e5de5718d59
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-19 08:28:31 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -3,12 +3,24 @@
 const canvas1 = document.getElementById('canvas1');
 const container1 = document.getElementById('container1');
 const ctx1 = canvas1.getContext('2d');
+console.log(ctx1);
 
 canvas1.width = container1.offsetWidth;
 canvas1.height = container1.offsetHeight;
 
-ctx1.fillStyle = 'red';
-ctx1.beginPath();
-ctx1.arc(50, 50, 50, 0, Math.PI * 2);
-ctx1.closePath();
-ctx1.fill();
+let size = 0;
+
+function animate1() {
+    size++;
+    ctx1.fillStyle = 'red';
+    ctx1.strokeStyle = 'black';
+    ctx1.lineWidth = 5;
+    ctx1.beginPath();
+    ctx1.arc(150, 150, size, 0, Math.PI * 2);
+    ctx1.closePath();
+    ctx1.fill();
+    ctx1.stroke();
+    requestAnimationFrame(animate1);
+}
+
+animate1();
