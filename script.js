f3bf5ed72665c2107b9b5612893a8dccd0e9161f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-19 09:03:06 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ console.log(ctx1);
 canvas1.width = container1.offsetWidth;
 canvas1.height = container1.offsetHeight;
 
-let number = 0;
-let scale = 10;
+let number1 = 0;
+let scale1 = 10;
 
-function drawFlower() {
-    let angle = number * 1;
-    let radius = scale * Math.sqrt(number);
+function drawFlower1() {
+    let angle = number1 * 2.5;
+    let radius = scale1 * Math.sqrt(number1);
     let positionX = radius * Math.sin(angle) + canvas1.width/2;
     let positionY = radius * Math.cos(angle) + canvas1.height/2;
 
@@ -26,14 +26,52 @@ function drawFlower() {
     ctx1.fill();
     ctx1.stroke();
 
-    number++;
+    number1++;
 }
 
 function animate1() {
     //ctx1. clearRect(0, 0, canvas1.width, canvas1.height);
-    //size += 0.05;
-    drawFlower();
+    drawFlower1();
     requestAnimationFrame(animate1);
 }
 
 animate1();
+
+//canvas2
+
+const canvas2 = document.getElementById('canvas2');
+const container2 = document.getElementById('container2');
+const ctx2 = canvas2.getContext('2d');
+console.log(ctx2);
+
+canvas2.width = container2.offsetWidth;
+canvas2.height = container2.offsetHeight;
+
+let number2 = 0;
+let scale2 = 10;
+
+function drawFlower2() {
+    let angle = number2 * 6;
+    let radius = scale2 * Math.sqrt(number2);
+    let positionX = radius * Math.sin(angle) + canvas2.width/2;
+    let positionY = radius * Math.cos(angle) + canvas2.height/2;
+
+    ctx2.fillStyle = 'red';
+    ctx2.strokeStyle = 'black';
+    ctx2.lineWidth = 5;
+    ctx2.beginPath();
+    ctx2.arc(positionX, positionY, 20, 0, Math.PI * 2);
+    ctx2.closePath();
+    ctx2.fill();
+    ctx2.stroke();
+
+    number2++;
+}
+
+function animate2() {
+    //ctx2. clearRect(0, 0, canvas2.width, canvas2.height);
+    drawFlower2();
+    requestAnimationFrame(animate2);
+}
+
+animate2();
