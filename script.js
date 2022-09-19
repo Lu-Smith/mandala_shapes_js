fb0b2580dedf7d421ab2b72735a7123708bff119
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-19 08:39:21 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -8,22 +8,28 @@ console.log(ctx1);
 canvas1.width = container1.offsetWidth;
 canvas1.height = container1.offsetHeight;
 
-let size = 0;
-let positionX = 0;
-let positionY = 0;
+let positionX = 100;
+let positionY = 100;
+let angle = 0;
 
-function animate1() {
-    size += 0.05;
-    positionX += 0.5;
-    positionY++;
+function drawFlower() {
     ctx1.fillStyle = 'red';
     ctx1.strokeStyle = 'black';
     ctx1.lineWidth = 5;
     ctx1.beginPath();
-    ctx1.arc(positionX, positionY, size, 0, Math.PI * 2);
+    ctx1.arc(positionX, positionY, 20, 0, Math.PI * 2);
     ctx1.closePath();
     ctx1.fill();
     ctx1.stroke();
+}
+
+function animate1() {
+    ctx1. clearRect(0, 0, canvas1.width, canvas1.height);
+    //size += 0.05;
+    positionX += 2 * Math.sin(angle);
+    positionY += 2 * Math.cos(angle);
+    angle += 0.1;
+    drawFlower();
     requestAnimationFrame(animate1);
 }
 
