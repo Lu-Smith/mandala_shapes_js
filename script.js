8226d31a90368ce343a063b2d34a1ba0d7f7cc77
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-19 08:07:03 +0100

    Updated script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//canvas1
+
+const canvas1 = document.getElementById('canvas1');
+const container1 = document.getElementById('container1');
+const ctx = canvas1.getContext('2d');
+
+canvas1.width = container1.innerWidth;
+canvas1.height = container1.innerHeight;
+
