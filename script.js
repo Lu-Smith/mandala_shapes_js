d8d9017d70fc50be0e1e71e0ba12d24d959d19dc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-19 08:20:23 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 
 const canvas1 = document.getElementById('canvas1');
 const container1 = document.getElementById('container1');
-const ctx = canvas1.getContext('2d');
+const ctx1 = canvas1.getContext('2d');
 
-canvas1.width = container1.innerWidth;
-canvas1.height = container1.innerHeight;
+canvas1.width = container1.offsetWidth;
+canvas1.height = container1.offsetHeight;
 
+ctx1.fillStyle = 'red';
+ctx1.beginPath();
+ctx1.arc(50, 50, 50, 0, Math.PI * 2);
+ctx1.closePath();
+ctx1.fill();
